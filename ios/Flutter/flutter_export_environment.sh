8b30ad27361da7dd8665ff249191ad6a56b3fe7c
Susmith4710 <157313637+Susmith4710@users.noreply.github.com>
2025-01-30 18:55:40 -0600

    ios changes

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/smeesa/Developer/flutter 2"
+export "FLUTTER_APPLICATION_PATH=/Users/smeesa/Documents/GitHub/idd-flutter/identity-defense-flutter/idd_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/smeesa/Documents/GitHub/idd-flutter/identity-defense-flutter/idd_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=ZW52PXRlc3Q=,dGVzdHVzZXI9Y29tcGxldGVmYW1pbHkrdGVzdHVzZXIxQHlvcG1haWwuY29t,dGVzdHB3ZD1UZXN0QDEyMw==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/smeesa/Documents/GitHub/idd-flutter/identity-defense-flutter/idd_app/.dart_tool/package_config.json"
